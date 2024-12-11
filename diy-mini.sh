86fbf76ac1ffb90b14d79920fa05d3a195bd6778
JiQuMao <66835939+JiQuMao@users.noreply.github.com>
2024-12-12 00:35:09 +0800

    Update diy-mini.sh

--- diy-mini.sh
+++ diy-mini.sh
@@ -118,15 +118,6 @@ sed -i 's|admin\\|admin\\/services\\|g' feeds/luci/applications/luci-app-dockerm
 # 取消对 samba4 的菜单调整
 # sed -i '/samba4/s/^/#/' package/lean/default-settings/files/zzz-default-settings
 
-# 关闭ipv6:
-cat >> .configs <<EOF
-CONFIG_KERNEL_IPV6=n
-CONFIG_KERNEL_IPV6_MULTIPLE_TABLES=n
-CONFIG_KERNEL_IPV6_SUBTREES=n
-CONFIG_KERNEL_IPV6_MROUTE=n
-CONFIG_IPV6=n
-EOF
-
 ./scripts/feeds update -a
 ./scripts/feeds install -a
 
